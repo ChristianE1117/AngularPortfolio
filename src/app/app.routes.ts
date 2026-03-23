3b36125aeb25d7fcf9c4dc07916ba5c933727ea5
ChristianE1117 <158534957+ChristianE1117@users.noreply.github.com>
2026-03-22 21:24:52 -0500

    initial commit

--- src/app/app.routes.ts
+++ src/app/app.routes.ts
@@ -1,3 +1,15 @@
 import { Routes } from '@angular/router';
+import { Home } from './home/home';
+import { About } from './about/about';
+import { Contact } from './contact/contact';
+import { Projects } from './projects/projects';
+import { WorkExperience } from './workExperience/workExperience';
 
-export const routes: Routes = [];
+export const routes: Routes = [
+    { path: 'home', component: Home },
+    { path: '', component: Home },
+    { path: 'about', component: About },
+    { path: 'contact', component: Contact },
+    { path: 'projects', component: Projects },
+    { path: 'workExperience', component: WorkExperience },
+];
---
+++ src/app/workExperience/workExperience.ts
@@ -0,0 +1,9 @@
+import { Component } from '@angular/core';
+
+@Component({
+  selector: 'app-workExperience',
+  imports: [],
+  templateUrl: './workExperience.html',
+  styleUrl: './workExperience.css',
+})
+export class WorkExperience {}
